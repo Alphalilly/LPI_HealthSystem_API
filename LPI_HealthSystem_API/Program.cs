73c2ebee7530230e5363449ac884e20f864ebaa5
Alphalilly <zoozoosamak@gmail.com>
2021-10-20 15:21:04 -0300

    Did some weird stuff with spill over
    and other stuff. just chugging away!

--- LPI_HealthSystem_API/Program.cs
+++ LPI_HealthSystem_API/Program.cs
@@ -20,24 +20,37 @@ namespace LPI_HealthSystem_API
 		//             > create sub-methods
 		//             > etc...
 
-		static int health;
-		static int shield;
-		static int lives;
+		static int health; //make a system for health
+		static int shield; //make a system for shield
+		static int lives; //make a system for lives (""""API""""???)
 
 		static int damage;
-		static int spillOver;
+		
 
-		static void Main(string[] args)
+		static void Main()
         {
 			DefultValues();
 
 			ShowHUD();
 
-			
+			TakeDamage(-123333322);
+
+			ShowHUD();
 
 
 			Console.ReadKey(true);
         }
+
+		static void test(int dmg)
+        {
+			if (dmg < 0)
+			{
+				//dmg = 0; // no u
+				//Console.WriteLine("error!");
+				return;
+			}
+
+		}
 
 		static void DefultValues()
         {
@@ -46,6 +59,99 @@ namespace LPI_HealthSystem_API
 			lives = 3;
 			damage = 0;
         }
+
+		static void DecLives() //not work?
+		{
+			if (lives == 0)
+			{
+				Console.WriteLine("Game Over"); //this does nothing
+			}
+			else if (lives < 0)
+			{
+				lives = lives - 1;
+				Console.WriteLine("Revive");
+				health = 100;
+				shield = 100;
+			}
+			
+		}
+
+		static int SpillOver(int dmg,int sp)
+        {
+			sp = dmg - shield;
+			health = health - sp;
+
+			return sp;
+        }
+
+		static void TakeDamage(int dmg) // lol fix this
+        {
+			//yandere dev on the scene!
+
+            if (dmg < 0)
+            {
+				//dmg = 0; // no u
+				Console.WriteLine("error!");
+				return;
+			}
+
+            else if (shield > 0) //extra mile: instead of putting 0 here. Have a defult min, max value.
+            {
+				shield = shield - dmg;
+            }
+
+			else if (dmg > shield)
+            {
+				SpillOver(dmg, dmg);
+				shield = 0;
+            }
+
+			else if (health > 0)
+            {
+				health = health - dmg;
+			}
+
+			else if (health <= 0)
+            {
+				health = 0;
+				DecLives(); // is this broken? T^T
+            }
+            // or I could have just wrapped all of this ^^^^ in		if (dmg < 0)	but me small brain :D
+
+		}
+
+		static void Heal(int hp)
+        {
+			if (hp < 0)
+			{
+				hp = 0; // no u
+				Console.WriteLine("error!");
+			}
+
+			health = health + hp;
+
+			if (health > 100)
+            {
+				health = 100;
+            }
+		}
+
+		static void RegenShield(int hp)
+        {
+			if (hp < 0)
+			{
+				hp = 0; // no u
+				Console.WriteLine("error!");
+			}
+
+			shield = shield + hp;
+
+			if (shield > 100)
+            {
+				shield = 100;
+            }
+		}
+
 
 		static void HealthStatus()
 		{
@@ -108,77 +214,6 @@ namespace LPI_HealthSystem_API
 			}
 		}
 
-		static void TakeDamage(int dmg) // lol fix this
-        {
-			if (dmg < 0)
-			{
-				Console.WriteLine("error!");
-			}
-
-			if (shield > 0) //extra mile: instead of putting 0 here. Have a defult min, max value.
-            {
-				shield = shield - dmg;
-            }
-
-			if (dmg > shield)
-            {
-				spillOver = dmg - shield;
-				health = health - spillOver;
-				shield = 0;
-            }
-
-			if (health > 0)
-            {
-				health = health - dmg;
-			}
-
-			if (health <= 0)
-            {
-				health = 0;
-				DecLives();
-            }
-
-		}
-
-		static void Heal(int hp)
-        {
-			if (hp < 0)
-			{
-				Console.WriteLine("error!");
-			}
-
-			health = health + hp;
-
-			if (health > 100)
-            {
-				health = 100;
-            }
-		}
-
-		static void RegenShield(int hp)
-        {
-			if (hp < 0)
-			{
-				Console.WriteLine("error!");
-			}
-
-			shield = shield + hp;
-
-			if (shield > 100)
-            {
-				shield = 100;
-            }
-		}
-
-		static void DecLives() //not work?
-        {
-			lives = lives - 1;
-			if (lives == 0)
-            {
-				Console.WriteLine("Game Over");
-            }
-		}
-
 		static void ShowHUD()
 		{
 			Console.ForegroundColor = ConsoleColor.DarkCyan;
@@ -187,7 +222,7 @@ namespace LPI_HealthSystem_API
 
 			Console.ForegroundColor = ConsoleColor.Magenta;
 
-			Console.WriteLine($"§\tDumb Protagonist\t§");
+			Console.WriteLine($"§\tPlayer\t§");
 
 			Console.ForegroundColor = ConsoleColor.DarkCyan;
 
