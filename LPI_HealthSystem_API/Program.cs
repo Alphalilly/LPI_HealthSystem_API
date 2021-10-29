b5d600af020e75c92d613c101c32e8d5bdb5897e
Alphalilly <zoozoosamak@gmail.com>
2021-10-29 10:56:57 -0300

    Added more "UnitTests" lel

--- LPI_HealthSystem_API/Program.cs
+++ LPI_HealthSystem_API/Program.cs
@@ -45,147 +45,359 @@ namespace LPI_HealthSystem_API
 
 		public void Dubug()
 		{
+			Console.ForegroundColor = ConsoleColor.Green;
 			Console.WriteLine("<?> Debug Mode <?>");
+			Console.ForegroundColor = ConsoleColor.White;
 			Console.WriteLine(" ");
 
-			// Assigning Defualt Values to the Variables
-			DefaultValues();
+			// ---------------------------------------------------------------------------------------------
+			// Tests if the default values have been set correctly
+
+			Console.ForegroundColor = ConsoleColor.Green;
 			Console.WriteLine("<?> Testing DefaultValues() ... ");
-            Debug.Assert(Health == 100, "You can put strings in here", "they dont do anything.");
+			Console.ForegroundColor = ConsoleColor.White;
+
+			DefaultValues();
+			Debug.Assert(Health == 100, "You can put strings in here", "they dont do anything."); // Debug
 			Debug.Assert(Shield == 100);
 			Debug.Assert(Lives == 3);
 			Debug.Assert(Damage == 0);
-			Console.WriteLine("<?> Test Successful!");
-			Console.WriteLine();
 
-			Console.WriteLine("<?> Testing TakeDamage() Clamping ... ");
-			Damage = -10;
-			TakeDamage(Damage);
-			Debug.Assert(Health == 100);
-			Debug.Assert(Shield == 100);
+			Console.ForegroundColor = ConsoleColor.Green;
+			Console.WriteLine("<?> Test Successful!");
+			Console.ForegroundColor = ConsoleColor.White;
+			Console.WriteLine();
+			// ---------------------------------------------------------------------------------------------
+
+
+			// ---------------------------------------------------------------------------------------------
+			// Tests if the ShowHUD() displays correctly
+
+			Console.ForegroundColor = ConsoleColor.Green;
+			Console.WriteLine("<?> Testing ShowHUD() ... ");
+			Console.ForegroundColor = ConsoleColor.White;
+
+			ShowHUD();
+
+			Console.ForegroundColor = ConsoleColor.Green;
+			Console.WriteLine("<?> Test Successful!");
+			Console.ForegroundColor = ConsoleColor.White;
+			Console.WriteLine();
+			// ---------------------------------------------------------------------------------------------
+
+
+			// ---------------------------------------------------------------------------------------------
+			// Tests that the TakeDamage() handles negative values correctly
+
+			Console.ForegroundColor = ConsoleColor.Green;
+			Console.WriteLine("<?> Testing TakeDamage() Clamping ... "); // clamping is probably not the right word.
+			Console.ForegroundColor = ConsoleColor.White;
+
+			Damage = -10; 
+			TakeDamage(Damage); 
+			Debug.Assert(Health == 100); 
+			Debug.Assert(Shield == 100); 
 			Debug.Assert(Lives == 3);
-			Console.WriteLine("<?> Test Successful!");
-			Console.WriteLine();
 
-			Console.WriteLine("<?> Testing TakeDamage() Shield ...");
+			Console.ForegroundColor = ConsoleColor.Green;
+			Console.WriteLine("<?> Test Successful!");
+			Console.ForegroundColor = ConsoleColor.White;
+			Console.WriteLine();
+			// ---------------------------------------------------------------------------------------------
+
+
+			// ---------------------------------------------------------------------------------------------
+			// Tests that the TakeDamage() Damages Shield
+
+			Console.ForegroundColor = ConsoleColor.Green;
+			Console.WriteLine("<?> Testing TakeDamage() on Shield ..."); 
+			Console.ForegroundColor = ConsoleColor.White;
+
 			Damage = 60;
 			TakeDamage(Damage);
 			Debug.Assert(Shield < 100);
-			Debug.Assert(Health <= 100);
-			Debug.Assert(Lives == 3);
-			Console.WriteLine("<?> Test Successful!");
-			Console.WriteLine();
 
-			Console.WriteLine("<?> Testing TakeDamage() Shield ...");
+			Console.ForegroundColor = ConsoleColor.Green;
+			Console.WriteLine("<?> Test Successful!");
+			Console.ForegroundColor = ConsoleColor.White;
+			Console.WriteLine();
+			// ---------------------------------------------------------------------------------------------
+
+
+			// ---------------------------------------------------------------------------------------------
+			// Tests that the RegenShield() handles negative values correctly
+
+			Console.ForegroundColor = ConsoleColor.Green;
+			Console.WriteLine("<?> Testing RegenShield() Clamping ..."); // clamping is probably not the right word
+			Console.ForegroundColor = ConsoleColor.White;
+
 			RegenShield(-20);
 			Debug.Assert(Shield < 100);
-			Debug.Assert(Health <= 100);
-			Debug.Assert(Lives == 3);
+
+			Console.ForegroundColor = ConsoleColor.Green;
 			Console.WriteLine("<?> Test Successful!");
+			Console.ForegroundColor = ConsoleColor.White;
 			Console.WriteLine();
+			// ---------------------------------------------------------------------------------------------
 
-			Console.ReadKey(true);
 
-			ShowHUD();
+			// ---------------------------------------------------------------------------------------------
+			// Tests that the RegenShield() applies regen to Shield, and tests Clamping
 
-			Console.ReadKey(true);
+			Console.ForegroundColor = ConsoleColor.Green;
+			Console.WriteLine("<?> Testing RegenShield() on Shield, and Clamping ...");
+			Console.ForegroundColor = ConsoleColor.White;
 
 			RegenShield(100);
+			Debug.Assert(Shield <= 100);
 
-			Console.ReadKey(true);
+			Console.ForegroundColor = ConsoleColor.Green;
+			Console.WriteLine("<?> Test Successful!");
+			Console.ForegroundColor = ConsoleColor.White;
+			Console.WriteLine();
+			// ---------------------------------------------------------------------------------------------
 
-			ShowHUD();
 
-			Console.ReadKey(true);
+			// ---------------------------------------------------------------------------------------------
+			// Tests That the TakeDamage() handles Damage spillover into health
+
+			Console.ForegroundColor = ConsoleColor.Green;
+			Console.WriteLine("<?> Testing TakeDamage() Spillover ...");
+			Console.ForegroundColor = ConsoleColor.White;
 
 			Damage = 120;
-
 			TakeDamage(Damage);
+			Debug.Assert(Shield == 0);
+			Debug.Assert(Health < 100);
 
-			Console.ReadKey(true);
+			Console.ForegroundColor = ConsoleColor.Green;
+			Console.WriteLine("<?> Test Successful!");
+			Console.ForegroundColor = ConsoleColor.White;
+			Console.WriteLine();
+			// ---------------------------------------------------------------------------------------------
 
-			ShowHUD();
 
-			Console.ReadKey(true);
+			// ---------------------------------------------------------------------------------------------
+			// Tests that the Heal() handles negative values correctly
+
+			Console.ForegroundColor = ConsoleColor.Green;
+			Console.WriteLine("<?> Testing Heal() Clamping ..."); // clamping is probably not the right word
+			Console.ForegroundColor = ConsoleColor.White;
 
 			Heal(-30);
+			Debug.Assert(Health == Health);
 
-			Console.ReadKey(true);
+			Console.ForegroundColor = ConsoleColor.Green;
+			Console.WriteLine("<?> Test Successful!");
+			Console.ForegroundColor = ConsoleColor.White;
+			Console.WriteLine();
+			// ---------------------------------------------------------------------------------------------
 
-			ShowHUD();
 
-			Console.ReadKey(true);
+			// ---------------------------------------------------------------------------------------------
+			// Tests that the Heal() applies hp to Health, and tests Clamping
+
+			Console.ForegroundColor = ConsoleColor.Green;
+			Console.WriteLine("<?> Testing Heal() on Health, and Clamping ...");
+			Console.ForegroundColor = ConsoleColor.White;
 
 			Heal(30);
+			Debug.Assert(Health > 80);
+			Debug.Assert(Health >= 100);
 
-			Console.ReadKey(true);
+			Console.ForegroundColor = ConsoleColor.Green;
+			Console.WriteLine("<?> Test Successful!");
+			Console.ForegroundColor = ConsoleColor.White;
+			Console.WriteLine();
+			// ---------------------------------------------------------------------------------------------
 
-			ShowHUD();
 
-			Console.ReadKey(true);
+			// ---------------------------------------------------------------------------------------------
+			// Tests that the TakeDamage() Damages Health, and clamps the value.
+
+			Console.ForegroundColor = ConsoleColor.Green;
+			Console.WriteLine("<?> Testing TakeDamage() on Health, and clamping ...");
+			Console.ForegroundColor = ConsoleColor.White;
 
 			Damage = 120;
-
 			TakeDamage(Damage);
+			Debug.Assert(Health <= 0);
 
-			Console.ReadKey(true);
+			Console.ForegroundColor = ConsoleColor.Green;
+			Console.WriteLine("<?> Test Successful!");
+			Console.ForegroundColor = ConsoleColor.White;
+			Console.WriteLine();
+			// ---------------------------------------------------------------------------------------------
 
-			ShowHUD();
 
-			Console.ReadKey(true);
+			// ---------------------------------------------------------------------------------------------
+			// Tests that the Revive() decriments Lives and sets Health and Shield back to max
+
+			Console.ForegroundColor = ConsoleColor.Green;
+			Console.WriteLine("<?> Testing Revive() ...");
+			Console.ForegroundColor = ConsoleColor.White;
 
 			Revive();
+			Debug.Assert(Lives < 3);
+			Debug.Assert(Health == 100);
+			Debug.Assert(Shield == 100);
 
-			Console.ReadKey(true);
 
-			ShowHUD();
+			Console.ForegroundColor = ConsoleColor.Green;
+			Console.WriteLine("<?> Test Successful!");
+			Console.ForegroundColor = ConsoleColor.White;
+			Console.WriteLine();
+			// ---------------------------------------------------------------------------------------------
 
-			Console.ReadKey(true);
+
+
+			// ---------------------------------------------------------------------------------------------
+			// Tests that the Revive() Displays a "Game Over" when Lives are depleated
+
+			Console.ForegroundColor = ConsoleColor.Green;
+			Console.WriteLine("<?> Testing Revive() Game Over ...");
+			Console.ForegroundColor = ConsoleColor.White;
 
 			Damage = 200;
-
 			TakeDamage(Damage);
-
-			Console.ReadKey(true);
-
-			ShowHUD();
-
-			Console.ReadKey(true);
-
+			Revive();
+			TakeDamage(Damage);
+			Revive();
+			TakeDamage(Damage);
 			Revive();
 
-			Console.ReadKey(true);
+			Debug.Assert(Lives == 0);
+			Debug.Assert(Health == 0);
+			Debug.Assert(Shield == 0);
 
-			ShowHUD();
+			Console.ForegroundColor = ConsoleColor.Green;
+			Console.WriteLine("<?> Test Successful!");
+			Console.ForegroundColor = ConsoleColor.White;
+			Console.WriteLine();
+			// ---------------------------------------------------------------------------------------------
 
-			Console.ReadKey(true);
-
-			TakeDamage(Damage);
-
-			ShowHUD();
-
-			Console.ReadKey(true);
-
-			Revive();
-
-			Console.ReadKey(true);
-
-			ShowHUD();
-
-			Console.ReadKey(true);
-
-			TakeDamage(Damage);
-
-			Console.ReadKey(true);
-
-			ShowHUD();
-
-			Console.ReadKey(true);
-
-			Revive();
-
-			Console.ReadKey(true);
+			Console.ForegroundColor = ConsoleColor.Green;
+			Console.WriteLine("<?> End of Test <?>");
+			Console.ForegroundColor = ConsoleColor.White;
+			Console.WriteLine(" ");
 		}
+
+		public void DefaultValues()
+		{
+
+			Console.WriteLine("<;> Defult Values set.");
+
+			Health = 100;
+			Shield = 100;
+			Lives = 3;
+			Damage = 0;
+		}
+
+		public void Revive()
+		{
+			if (Lives <= 0)
+			{
+				Lives = 0;
+				Console.WriteLine("> Game Over");
+				return;
+			}
+			if (Lives > 0)
+			{
+				Lives--;
+				Console.WriteLine("> Revive");
+				Health = 100;
+				Shield = 100;
+			}
+
+		}
+
+		public void TakeDamage(int dmg)
+		{
+			int spillOver;
+
+			if (dmg < 0)
+			{
+				Console.ForegroundColor = ConsoleColor.Red;
+				Console.WriteLine("<!> Error! Damage = " + dmg + ". Value Must be positive.");
+				Console.ForegroundColor = ConsoleColor.White;
+
+				return;
+			}
+
+			if (Shield > 0)
+            {
+                if (dmg > Shield)
+                {
+                    Console.WriteLine("> Player is dealt " + dmg + " Damage! The difference spills over into health");
+                    spillOver = dmg - Shield;
+                    Health -= spillOver;
+                    Shield = 0;
+                }
+                else
+                {
+					Console.WriteLine("> Player is dealt " + dmg + " Damage!");
+
+					Shield -= dmg;
+                }
+            }
+            else
+            {
+				Console.WriteLine("> Player is dealt " + dmg + " Damage!");
+
+				Health -= dmg;
+			}
+
+            if (Health <= 0)
+            {
+				Health = 0;
+				Console.WriteLine("> Health is clamped at " + Health);
+			}
+		}
+
+		public void Heal(int hp)
+		{
+			if (hp < 0)
+			{
+				Console.ForegroundColor = ConsoleColor.Red;
+				Console.WriteLine("<!> Error! Heal = " + hp + ". Value Must be positive.");
+				Console.ForegroundColor = ConsoleColor.White;
+
+				return;
+			}
+
+			Console.WriteLine("> Player Heals " + hp + " Health!");
+
+			Health = Health + hp;
+
+			if (Health > 100)
+			{
+				Health = 100;
+				Console.WriteLine("> Player Health is clamped at " + Health);
+			}
+		}
+
+		public void RegenShield(int hp)
+		{
+			if (hp < 0)
+			{
+				Console.ForegroundColor = ConsoleColor.Red;
+				Console.WriteLine("<!> Error! Regen = " + hp + ". Value Must be positive");
+				Console.ForegroundColor = ConsoleColor.White;
+
+				return;
+			}
+
+			Console.WriteLine("> Player Regenerates " + hp + " Sheild!");
+
+			Shield = Shield + hp;
+
+			if (Shield > 100)
+			{
+				Shield = 100;
+				Console.WriteLine("> Player Shield is clamped at " + Shield);
+			}
+		}
+
 
 		void HealthStatus()
 		{
@@ -219,7 +431,7 @@ namespace LPI_HealthSystem_API
 			}
 		}
 
-		void Boarder(float increment) 
+		void Boarder(float increment)
 		{
 			if (increment == 1)
 			{
@@ -299,120 +511,6 @@ namespace LPI_HealthSystem_API
 			else
 			{
 				return;
-			}
-		}
-
-
-		public void DefaultValues()
-		{
-
-			Console.WriteLine("<;> Defult Values set.");
-
-			Health = 100;
-			Shield = 100;
-			Lives = 3;
-			Damage = 0;
-		}
-
-		public void Revive()
-		{
-			if (Lives <= 0)
-			{
-				Lives = 0;
-				Console.WriteLine("> Game Over");
-				return;
-			}
-			if (Lives > 0)
-			{
-				Lives--;
-				Console.WriteLine("> Revive");
-				Health = 100;
-				Shield = 100;
-			}
-
-		}
-
-		public void TakeDamage(int dmg)
-		{
-			int spillOver;
-
-			if (dmg < 0)
-			{
-				Console.ForegroundColor = ConsoleColor.Red;
-				Console.WriteLine("<!> Error! Damage = " + dmg + ". Value Must be positive.");
-				Console.ForegroundColor = ConsoleColor.White;
-
-				return;
-			}
-
-			if (Shield > 0)
-            {
-                if (dmg > Shield)
-                {
-                    Console.WriteLine("> Player is dealt " + dmg + " Damage!");
-                    spillOver = dmg - Shield;
-                    Health -= spillOver;
-                    Shield = 0;
-                }
-                else
-                {
-					Console.WriteLine("> Player is dealt " + dmg + " Damage!");
-
-					Shield -= dmg;
-                }
-            }
-            else
-            {
-				Console.WriteLine("> Player is dealt " + dmg + " Damage!");
-
-				Health -= dmg;
-			}
-
-            if (Health <= 0)
-            {
-				Health = 0;
-            }
-        }
-
-		public void Heal(int hp)
-		{
-			if (hp < 0)
-			{
-				Console.ForegroundColor = ConsoleColor.Red;
-				Console.WriteLine("<!> Error! Heal = " + hp + ". Value Must be positive.");
-				Console.ForegroundColor = ConsoleColor.White;
-
-				return;
-			}
-
-			Console.WriteLine("> Player Heals" + hp + " Health!");
-
-			Health = Health + hp;
-
-			if (Health > 100)
-			{
-				Health = 100;
-			}
-		}
-
-		public void RegenShield(int hp)
-		{
-			if (hp < 0)
-			{
-				Console.ForegroundColor = ConsoleColor.Red;
-				Console.WriteLine("<!> Error! Regen = " + hp + ". Value Must be positive");
-				Console.ForegroundColor = ConsoleColor.White;
-
-				return;
-			}
-
-			Console.WriteLine("> Player Regenerates " + hp + " Sheild!");
-
-			Shield = Shield + hp;
-
-			if (Shield > 100)
-			{
-				Shield = 100;
 			}
 		}
 
